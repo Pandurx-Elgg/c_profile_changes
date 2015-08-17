91a039c6a3c3bb6b733cf72c7572a1bbfe6be2e3
Christine Yu <internalfire5@live.com>
2015-08-17 07:36:39 -0700

    first commit to this module

---
+++ languages/fr.php
@@ -0,0 +1,9 @@
+<?php
+
+$french = array(
+    "gcc_profile:error" => "N'a pas sauver: ",
+    "gcc_profile:missingemail" => "Aucun courriel fournie",
+    "gcc_profile:notaccepted" => "Aucune email du gouvernement prévu"
+);
+
+add_translation("fr",$french);
